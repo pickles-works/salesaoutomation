f4be24ff5bed15567f00132b1c572cfd2d2345bb
asamottoo-works <asamottoo.works@gmail.com>
2022-03-07 05:50:46 +0900

    add flyway

---
+++ database/src/main/resources/db/migration/V1_0_0_1__create_tables.sql
@@ -0,0 +1,57 @@
+-- アカウント
+
+create schema if not exists accounts;
+
+create table accounts.accounts (
+    id uuid not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id)
+);
+
+create table accounts.account_revisions (
+    id uuid not null,
+    revision bigint not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id, revision),
+    foreign key (id) references accounts.accounts (id)
+);
+
+create table accounts.latest_accounts (
+    id uuid not null,
+    revision bigint not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id),
+    foreign key (id) references accounts.accounts (id),
+    foreign key (id, revision) references accounts.account_revisions (id, revision)
+);
+
+create table accounts.account_revision_values (
+    id uuid not null,
+    revision bigint not null,
+    email varchar(1024) not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id, revision),
+    foreign key (id, revision) references accounts.account_revisions (id, revision)
+);
+
+create table accounts.account_revision_counts (
+    id uuid not null,
+    revision bigint not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id),
+    foreign key (id) references accounts.accounts (id)
+);
+
+create table accounts.locked_accounts(
+    id uuid not null,
+    primary key (id),
+    created_at timestamp not null default current_timestamp,
+    foreign key (id) references accounts.accounts (id)
+);
+
+create table accounts.deleted_accounts(
+    id uuid not null,
+    created_at timestamp not null default current_timestamp,
+    primary key (id),
+    foreign key (id) references accounts.accounts (id)
+);
